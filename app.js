34092fcd91484376617e50c317381af83c6cbec5
Mohammad Faizan <mdfaizan08iitp@gmail.com>
2024-07-06 02:50:56 +0530

    Storing data using mongodb database

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-//jshint esversion:6
 
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import ejs from "ejs";
 import _ from "lodash";
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts=[];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,12 +15,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
+const postSchema={
+   title:String,
+   content:String
+};
 
+const Post=mongoose.model("Post",postSchema);
 
-app.get("/",function(req,res){
-   res.render("home",{introHome:homeStartingContent,content:posts});
+app.get("/",async function(req,res){
+
+   const Allposts=await Post.find({}); 
+   res.render("home",{introHome:homeStartingContent,content:Allposts});
+
 })
 
 
@@ -38,31 +46,26 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-    const post={
+    // A new Document using the post model.
+    const post=new Post({
         title:req.body.postTitle,
         content:req.body.postBody
-    };
-    posts.push(post);
+    });
+    post.save();
+ 
     res.redirect("/");
 });
 
- app.get("/posts/:postName",function(req,res){
-   const requestedTitle=_.lowerCase(req.params.postName);
-   let flag=0;
-     for(let i=0;i<posts.length;i++){
-        if(_.lowerCase(posts[i].title)===requestedTitle){
-            res.render("post",{
-              postTitle:posts[i].title,
-              postBody:posts[i].content
-            });
-          flag=1;
-          break;
-        }
-     }
-     if(flag==0){
-       console.log("Match Not Found");
-     } 
- });
+
+app.get("/posts/:postId",async function(req,res){
+   const requestedPostId=req.params.postId;
+   
+   const particularPost=await Post.findById(requestedPostId);
+   const postTitle=particularPost.title;
+   const postBody=particularPost.content;
+   res.render("post",{postTitle:postTitle,postBody:postBody});
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
